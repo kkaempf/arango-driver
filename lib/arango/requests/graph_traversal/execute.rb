15bc3677ea0ac0c7309868928a9b204ea6690062
Klaus Kämpf <kkaempf@gmail.com>
2023-03-14 09:15:56 +0100

    Document Arango::Requests::GraphTraversal classes
    Signed-off-by: Klaus Kämpf <kkaempf@gmail.com>

--- lib/arango/requests/graph_traversal/execute.rb
+++ lib/arango/requests/graph_traversal/execute.rb
@@ -1,8 +1,9 @@
 module Arango
   # Arango Requests
   module Requests
-  # Arango Requests
+    # GraphTraversal Requests
     module GraphTraversal
+      # API: POST {/dbcontext}/_api/traversal
       class Execute < Arango::Request
         self.request_method = :post
 
